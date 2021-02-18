847436f7e7b699ea1875b182e65eb54c6a7047bb
BtramVu <bbbvut@gmail.com>
2021-02-17 22:51:00 -0800

    add post routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,91 +1,89 @@
-// require What should I require here? 
-//
-module.exports = function (app){
+// // require workouts model form model 
+// const db = require("../models/excercise");
+// module.exports = function (app){
 
 
-// // get last workout, this routes come from api.js client side javascript 
-app.get("/api/workouts", (req,res)=>{
-  
-  db.Workouts.find({})
-  // you can call this anything but in this case it is dbWorkout
-  // 15 tell you to do this 
-  .then(dbWorkouts =>{
-    res.json(dbWorkouts)
-  })
-  .catch(err =>{
-    res.json(err);
-  });
-})
+//   // // get last workout, this routes come from api.js client side javascript 
+//   app.get("/api/workouts", (req,res)=>{
+    
+//     db.Workouts.find({})
+//     // you can call this anything but in this case it is dbWorkout
+//     // 15 tell you to do this 
+//     .then(dbWorkouts =>{
+//       res.json(dbWorkouts)
+//     })
+//     .catch(err =>{
+//       res.json(err);
+//     });
+//   });
 
-// 9 tell you to do this 
-app.get("/api/workouts", (req,res)=>{
-db.Workouts.find({}, (err, dbWorkouts)=>{
-  if(err){
-    console.log(err);
-  } else {
-    res.json(dbWorkouts)
-  }
-})
-}
-// it is ok to find all, because the clientside (api line 11)will serve up only last one 
-// add function 
+//   // // 9 tell you to do this 
+//   // app.get("/api/workouts", (req,res)=>{
+//   //   db.Workouts.find({}, (err, dbWorkouts)=>{
+//   //     if(err){
+//   //       console.log(err);
+//   //     } else {
+//   //       res.json(dbWorkouts)
+//   //     }
+//   //   });
+//   // });
+//   // it is ok to find all, because the clientside (api line 11)will serve up only last one 
+//   // add function 
 
-// target that id to put 
-// you are trying to add the work out that is already there
-// again, think of this as your "hobbies", there are many thigs in here, and you want to add now one in 
-app.put("/api/workouts/:id", (req,res)=>{
-  db.nameofCollection.update(
-    {
-      _id: mongojs.ObjectId(params.id)
-    },
-    {
-      $push //add workout to excersice array in model 
-    }, 
-    (err,edited)=>{
-      if(err){
-        console.log(err);
-        res.send(err);
-      } else{
-        console.log(edited);
-        // show this to the page
-        res.send(edited );
-      }
-    }
-);
-});
+//   // target that id to put 
+//   // you are trying to add the work out that is already there
+//   // again, think of this as your "hobbies", there are many thigs in here, and you want to add now one in 
+//   app.put("/api/workouts/:id", (req,res)=>{
+//     db.Workouts.findOneAndUpdate (
+//       {
+//         _id: mongojs.ObjectId(params.id)
+//       },
+//       {
+//         //add workout to excersice array in model 
+//         $push: { excersices: req.body }
+//       }, 
+//       { new: true })
+//       .then(dbWorkouts =>{
+//         res.json(dbWorkouts)
+//       })
+//       .catch(err =>{
+//         res.json(err);
+//       });
+      
+//   });
 
 
-// create new WORKOUT 
-  // the route you want to get 
-  app.post("/api/workouts", (req,res)=>{
-    create 
-    // what do you want to post- i want to post body
-    // what is in this body? 
-  req.body 
-  })
+//   // create new WORKOUT 
+//     // the route you want to get 
+//     app.post("/api/workouts", ( { body },res)=>{
+//       // create 
+//       // what do you want to post- i want to post body
+//       // what is in this body? 
+//       db.Workouts.create(body)
+//       .then(dbWorkouts =>{
+//         res.json(dbWorkouts)
+//       })
+//       .catch(err =>{
+//         res.json(err);
+//       });
 
-  // btrying
-  app.post("/api/workouts", ({ body },res)=>{
-    // what do you want to post- i want to post body
-    // what is in this body? 
-    const newWorkout = body;
-
-  })
+//     })
 
 
 
-// get workout in range 
-app.get("/api/workouts/range", (req, res)=>{
-  db.Workouts.find({})
-  .then(dbWorkouts =>{
-    res.json(dbWorkouts)
-  })
-  .catch(err =>{
-    res.json(err);
-  });
-})
 
-}//close module.export function 
+//   // get workout in range 
+//   app.get("/api/workouts/range", (req, res)=>{
+//     db.Workouts.find({})
+//     .then(dbWorkouts =>{
+//       res.json(dbWorkouts)
+//     })
+//     .catch(err =>{
+//       res.json(err);
+//     });
+//   });
+
+// }//close module.export function 
 
 
 // app.the request here(get/post/put/delete) ("then here is the route"), (req,res)=>{
@@ -111,3 +109,60 @@ app.get("/api/workouts/range", (req, res)=>{
 //     }//cái này là đóng cùa cái function err,data
 // )cái này là đóng cùa cái sự gì đó mình làm ở line 65
 // )} cái này là đóng của req,res 
+
+// let's try aagain 
+// 
+// 
+// 
+// require workouts model form model 
+const db = require("../models/excercise.js");
+module.exports = function (app){
+  app.get("/api/workouts", (req,res)=>{
+    db.Workouts.find({})
+    .then(dbWorkouts =>{
+      res.json(dbWorkouts)
+    })
+    .catch(err =>{
+      res.json(err);
+    });
+  });
+
+  app.put("/api/workouts/:id", (req,res)=>{
+    db.Workouts.findOneAndUpdate (
+      { _id: mongojs.ObjectId(params.id)
+      },
+      {
+        $push: { excersices: req.body }
+      }, 
+      { new: true })
+      .then(dbWorkouts =>{
+        res.json(dbWorkouts)
+      })
+      .catch(err =>{
+        res.json(err);
+      });   
+  });
+
+  // create new WORKOUT 
+    app.post("/api/workouts", ( { body },res)=>{
+      db.Workouts.create(body)
+      .then(dbWorkouts =>{
+        res.json(dbWorkouts)
+      })
+      .catch(err =>{
+        res.json(err);
+      });
+    })
+
+  // get workout in range 
+  app.get("/api/workouts/range", (req, res)=>{
+    db.Workouts.find({})
+    .then(dbWorkouts =>{
+      res.json(dbWorkouts)
+    })
+    .catch(err =>{
+      res.json(err);
+    });
+  });
+
+}
